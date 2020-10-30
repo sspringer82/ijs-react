97d9b6ba239fd3a4585a27e35ad1f0e4187f2f90
Sebastian Springer <info@sebastian-springer.com>
2020-10-30 13:27:34 +0100

    Backend implementation

--- README.md
+++ README.md
@@ -30,3 +30,19 @@ create-react-app Projectname --use-npm --template typescript
 ````npm install
 npm start```
 ````
+
+## Get a Backend
+
+1. `npm install json-server``
+2. create a backend script within the package.json file
+3. define a data.json-file at the root level of our application
+4. run `npm run backend`
+
+## Plan for the afternoon:
+
+- styling
+- more on hooks (custom hooks,...)
+- context api
+- form handling
+- central state management (redux...)
+- your questions
--- src/fetchData.ts
+++ src/fetchData.ts
@@ -1,22 +1,27 @@
 import Log from './shared/Log';
 
-export default function fetchData(): Promise<Log[]> {
-  return Promise.resolve([
-    {
-      id: 1,
-      from: '08:00',
-      until: '09:00',
-      title: 'Breakfast',
-      duration: 1,
-    },
-    {
-      id: 2,
-      from: '09:00',
-      until: '11:00',
-      title: 'Work',
-      duration: 2,
-    },
-  ]);
+export default async function fetchData(): Promise<Log[]> {
+  const response = await fetch('http://localhost:3001/logs');
+  const data = await response.json();
+
+  return data;
+
+  // return Promise.resolve([
+  //   {
+  //     id: 1,
+  //     from: '08:00',
+  //     until: '09:00',
+  //     title: 'Breakfast',
+  //     duration: 1,
+  //   },
+  //   {
+  //     id: 2,
+  //     from: '09:00',
+  //     until: '11:00',
+  //     title: 'Work',
+  //     duration: 2,
+  //   },
+  // ]);
 }
 
 // fetchData().then(data => {
